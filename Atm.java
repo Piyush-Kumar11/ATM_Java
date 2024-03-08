8b388b337c5bd10a9e845b1c665f2364da373d61
Piyush <piushverma111@gmail.com>
2024-03-08 12:43:45 +0530

    Updated Code

--- Atm.java
+++ Atm.java
@@ -1,3 +1,8 @@
+
+// Author: Piyush Kumar
+// Date: 08-Mar-2024
+// Description: This code describes about using Switch-Case, Do-While and Static methods inside a class to perform simple operations like an ATM Machine interface works.
+
 import java.util.Scanner;
 class Atm
 {
@@ -5,8 +10,9 @@ class Atm
 	public static void main(String args[])
 	{
 
-	Atm f1=new Atm();
-	f1.Z = 10500;
+	Atm f1=new Atm();	//Creating an object for the Global Variable
+	f1.Z = 10500;		
+
 	Scanner sc=new Scanner(System.in);	
 	char f;
 	do{
@@ -43,12 +49,13 @@ class Atm
 			System.out.print("Press Y/y to use our service again or other key to Exit:");
 			f=sc.next().charAt(0);
 
-		}while(f=='Y' || f=='y');
+		}while(f=='Y' || f=='y');		// Here do while loop terminates and ask for running the task again!!!
+
 		System.out.println("Thank You!!!");
 		System.out.println("\"WORLD BANK\"");
 	}
 
-	public static void depositMoney(int D)
+	public static void depositMoney(int D)		//Static method to deposit money
 	{	int sum=D;
 		Atm f1=new Atm();
 		f1.Z = 10500;
@@ -60,13 +67,13 @@ class Atm
 			System.out.println("Invalid Amount. Please enter a Positive value");}
 	}
 
-	public static void withdrawAmount(int C)
+	public static void withdrawAmount(int C)	//Static method to withdraw Amount
 	{
 		int sub=0;
 		Atm f1=new Atm();
 		f1.Z = 10500;
 
-		if(C>0 && C<=(f1.Z)){
+		if(C>0 && C<=(f1.Z)){			// Checks if the Amount is less than the available amount 
 			sub=(f1.Z)-C;
 			System.out.println("Withdrawal Successful. Remaining Balance is: "+sub);}
 		else{
